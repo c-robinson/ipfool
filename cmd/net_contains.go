fab524f1d05af49b5a257c7fcada6e613251eb17
Chad Robinson <chadr@zang.com>
2023-12-30 16:41:57 -0800

    implement 'net contains'

--- cmd/net_contains.go
+++ cmd/net_contains.go
@@ -2,7 +2,10 @@ package cmd
 
 import (
 	"fmt"
+	"net"
+	"os"
 
+	"github.com/c-robinson/iplib/v2"
 	"github.com/spf13/cobra"
 )
 
@@ -24,11 +27,35 @@ it kind of works out.`,
 	DisableFlagsInUseLine: true,
 	Args:                  cobra.ExactArgs(2),
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("unimplemented")
+		ipnet := retrieveIPNetwork(args[0], v46)
+
+		// if the compare request is against a valid network
+		_, cnet, err := iplib.ParseCIDR(args[1])
+		if err == nil {
+			respondToTrueFalseQuestion(ipnet.ContainsNet(cnet), ncCode)
+		}
+
+		caddr := net.ParseIP(args[1])
+		if caddr != nil {
+			respondToTrueFalseQuestion(ipnet.Contains(caddr), ncCode)
+		}
+
+		fmt.Println("Invalid input")
+		os.Exit(1)
 	},
 }
 
 func init() {
 	netRootCmd.AddCommand(netContainsCmd)
 	netContainsCmd.Flags().BoolVarP(&ncCode, "code", "x", false, "use exit code for output")
+}
+
+func respondToTrueFalseQuestion(result, exitCodeOnly bool) {
+	if !exitCodeOnly {
+		fmt.Printf("%t\n", result)
+	}
+	if result {
+		os.Exit(0)
+	}
+	os.Exit(1)
 }
