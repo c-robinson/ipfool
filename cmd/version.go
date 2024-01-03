086e38ea1e26aca688be9b531a63414bf8cc9472
Chad Robinson <chadr@zang.com>
2024-01-02 17:22:11 -0800

    added version information

--- cmd/version.go
+++ cmd/version.go
@@ -3,9 +3,20 @@ package cmd
 import (
 	"fmt"
 	"runtime/debug"
+	"sort"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
+
+type Stamp struct {
+	InfoGoVersion string
+	InfoCompiler  string
+	InfoGOARCH    string
+	InfoGOOS      string
+	InfoBuildTime string
+	VCSRevision   string
+}
 
 var versionExtendedFlag bool
 
@@ -21,15 +32,42 @@ var VersionCmd = &cobra.Command{
 			}
 		}
 
-		fmt.Printf("ipfool version %s (using iplib %s)\n", info.Main.Version, iplibVersion)
+		fmt.Printf("ipfool version %s, using iplib %s\n", info.Main.Version, iplibVersion)
 		if versionExtendedFlag {
-			getExtendedVersion(info)
+			viewExtendedVersion(info)
 		}
 	},
 }
 
-func getExtendedVersion(info *debug.BuildInfo) {
+func retrieveDepends(info *debug.BuildInfo) []string {
+	var name, ver string
 
+	Depends := []string{}
+
+	for _, module := range info.Deps {
+		path := strings.Split(module.Path, "/")
+		if len(path) < 3 {
+			return Depends
+		}
+		name = path[2]
+		if strings.Contains("golang.org", path[0]) {
+			return Depends
+		}
+
+		if len(module.Version) == 0 {
+			ver = module.Sum
+		} else {
+			ver = module.Version
+		}
+		Depends = append(Depends, fmt.Sprintf("%s %s", name, ver))
+	}
+
+	sort.Strings(Depends)
+	return Depends
+}
+
+func retrieveStamp(info *debug.BuildInfo) *Stamp {
+	stamp := Stamp{}
 	for _, setting := range info.Settings {
 		switch setting.Key {
 		case "-compiler":
@@ -45,13 +83,22 @@ func getExtendedVersion(info *debug.BuildInfo) {
 			stamp.InfoBuildTime = setting.Value
 
 		case "vcs.revision":
-			stamp.GameRevision = setting.Value
+			stamp.VCSRevision = setting.Value
 		}
 	}
+	return &stamp
+}
 
-	fmt.Printf("Built on %s using %s\n", info, info.Main.Path)
-	for _, mod := range info.Deps {
-		fmt.Printf("%s %s\n", mod.Path, mod.Version)
+func viewExtendedVersion(info *debug.BuildInfo) {
+	stamp := retrieveStamp(info)
+	depends := retrieveDepends(info)
+
+	fmt.Printf("  Built with %s on %s\n", stamp.InfoCompiler, stamp.InfoBuildTime)
+	fmt.Printf("  VCS revision: %s\n", stamp.VCSRevision)
+	fmt.Printf("  Go version %s, GOOS %s, GOARCH %s\n", info.GoVersion, stamp.InfoGOOS, stamp.InfoGOARCH)
+	fmt.Printf("  Dependencies:\n")
+	for _, mod := range depends {
+		fmt.Printf("    %s\n", mod)
 	}
 }
 
@@ -59,70 +106,3 @@ func init() {
 	rootCmd.AddCommand(VersionCmd)
 	VersionCmd.Flags().BoolVar(&versionExtendedFlag, "extended", false, "get expanded version information")
 }
-
-/*
-func RetrieveStamp() *Stamp {
-	info, _ := debug.ReadBuildInfo()
-
-	stamp := Stamp{}
-	stamp.Dependencies = []Dependency{}
-	for _, mod := range info.Deps {
-		dep, ok := retrieveDepends(mod)
-		if ok {
-			stamp.Dependencies = append(stamp.Dependencies, dep)
-		}
-	}
-	sort.Slice(stamp.Dependencies, func(i, j int) bool {
-		return stamp.Dependencies[i].Name < stamp.Dependencies[j].Name
-	})
-
-	stamp.InfoGoVersion = info.GoVersion
-	stamp.GameVersion = info.Main.Version
-	settings := info.Settings
-	for _, setting := range settings {
-		switch setting.Key {
-		case "-compiler":
-			stamp.InfoCompiler = setting.Value
-
-		case "GOARCH":
-			stamp.InfoGOARCH = setting.Value
-
-		case "GOOS":
-			stamp.InfoGOOS = setting.Value
-
-		case "vcs.time":
-			stamp.InfoBuildTime = setting.Value
-
-		case "vcs.revision":
-			stamp.GameRevision = setting.Value
-		}
-	}
-	return &stamp
-}
-
-func retrieveDepends(module *debug.Module) (Dependency, bool) {
-	var name, ver string
-
-	path := strings.Split(module.Path, "/")
-	if len(path) < 3 {
-		return Dependency{}, false
-	}
-	name = path[2]
-	if strings.Contains("golang.org", path[0]) {
-		return Dependency{}, false
-	}
-
-	if len(module.Version) == 0 {
-		ver = module.Sum
-	} else {
-		ver = module.Version
-	}
-	dep := Dependency{
-		Name:    name,
-		Version: ver,
-	}
-
-	return dep,
-}
-
-*/
